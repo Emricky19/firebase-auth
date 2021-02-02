7b38140d132477996b4aae4916c43a4f339b99f5
Emrick19 <emricky17@gmail.com>
2021-02-02 22:00:59 +0100

    Update AuthContext

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,33 +1,43 @@
-import React, { createContext, useState, useEffect, useContext } from 'react'
-import { auth } from '../config/firebase'
+import React, { createContext, useState, useEffect, useContext } from "react";
+import { auth } from "../config/firebase";
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
-const useAuth = () => useContext(AuthContext)
+const useAuth = () => useContext(AuthContext);
 
-const AuthProvider = ({children}) => {
-    const [currentUser, setCurrentUser] = useState()
+const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState();
 
-    const signup = (email, password) => auth.createUserWithEmailAndPassword(email, password)
+  const signup = (email, password) => auth.createUserWithEmailAndPassword(email, password);
 
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged(user => {
-            setCurrentUser(user)
-        })
+  const login = (email, password) => auth.signInWithEmailAndPassword(email, password);
 
-        return unsubscribe
-    }, [])
-    
-    const value = {
-        currentUser,
-        signup
-    }
+  const logout = () => auth.signOut();
 
-    return (
-        <AuthContext.Provider value={value}>
-            { children }
-        </AuthContext.Provider>
-    )
-}
+  const resetPassword = (emailAddress) => auth.sendPasswordResetEmail(emailAddress)
 
-export { AuthProvider, useAuth, }
+  const updateEmail = (email) => currentUser.updateEmail(email)
+  const updatePassword = (password) => currentUser.updatePassword(password)
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    signup,
+    login,
+    logout,
+    resetPassword,
+    updateEmail,
+    updatePassword
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export { AuthProvider, useAuth };
