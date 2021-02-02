60accf021a96fbe6fbd259710d99a2f88938703c
Emrick19 <emricky17@gmail.com>
2021-02-02 15:08:05 +0100

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { AuthProvider } from "./contexts/AuthContext";
+import Signup from "./components/Signup";
+
+
+import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
-    <div className="app">
-      <h1>Hello React App</h1>
-    </div>
-  )
-}
+    <AuthProvider>
+      <Container className="d-flex align-items-center justify-content-center min-vh-100">
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Signup />
+        </div>
+      </Container>
+    </AuthProvider>
+  );
+};
 
-export default App
+export default App;
