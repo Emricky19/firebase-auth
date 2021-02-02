34c28b224c9add91864f1912b81ca55572c6412a
Emrick19 <emricky17@gmail.com>
2021-02-02 21:59:58 +0100

    Update Signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,11 @@
 import React, { useRef, useState } from "react";
 import { Card, Button, Form, Alert } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
+import { Link, useHistory } from "react-router-dom";
 
 const Signup = () => {
-const [ error, setError ] = useState('')
-const [ loading, setloading ] = useState(false)
+  const [error, setError] = useState("");
+  const [loading, setloading] = useState(false);
 
   const emailRef = useRef();
   const passwordRef = useRef();
@@ -12,29 +13,32 @@ const [ loading, setloading ] = useState(false)
 
   const { signup } = useAuth();
 
+  const history = useHistory();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if(passwordRef.current.value !== confirmPasswordRef.current.value){
-        return setError('Passwords do not match')
+    if (passwordRef.current.value !== confirmPasswordRef.current.value) {
+      return setError("Passwords do not match");
     }
-    try{
-        setError('')
-        setloading(true)
-        await signup(emailRef.current.value, passwordRef.current.value);
-    }catch(err){
-        console.log(err);
-        setError('Failed to create account')
+    try {
+      setError("");
+      setloading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+      history.push('/')
+    } catch (err) {
+      console.log(err);
+      setError("Failed to create account");
     }
-    
-    setloading(false)
+
+    setloading(false);
   };
   return (
     <>
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Sign Up</h2>
-            {error && <Alert variant="danger">{error}</Alert>}
+          {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group controlId="email">
               <Form.Label>Email</Form.Label>
@@ -58,7 +62,7 @@ const [ loading, setloading ] = useState(false)
         </Card.Body>
       </Card>
       <div className="w-1000 text-center mt-2">
-        Already have an account? Log In
+        Already have an account? <Link to="/login">Log In</Link>
       </div>
     </>
   );
