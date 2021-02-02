ade6f89ae6ded0e037a5759e5d3b9fbae63800a7
Emrick19 <emricky17@gmail.com>
2021-02-02 21:56:38 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React from "react";
 import { AuthProvider } from "./contexts/AuthContext";
+import Dashboard from "./components/Dashboard";
 import Signup from "./components/Signup";
+import Login from "./components/Login";
 
+import ForgotPassword from "./components/ForgotPassword";
+import UpdateProfile from "./components/UpdateProfile"
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import PrivateRoute from "./components/PrivateRoute";
 
 import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
-    <AuthProvider>
-      <Container className="d-flex align-items-center justify-content-center min-vh-100">
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Signup />
-        </div>
-      </Container>
-    </AuthProvider>
+    <Container className="d-flex align-items-center justify-content-center min-vh-100">
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path="/" component={Dashboard} />
+              <PrivateRoute  path="/update-profile" component={UpdateProfile} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   );
 };
 
